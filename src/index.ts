1225d205cbc0c0f4dd1a7999ec69bad89a4958ec
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-03 19:34:25 +0900

    schemaを試す

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Require the framework and instantiate it
 
 import Fastify from 'fastify'
+import { FromSchema } from 'json-schema-to-ts';
 
 const fastify = Fastify({
   logger: true
@@ -14,6 +15,55 @@ const fastify = Fastify({
 fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' })
 })
+
+
+const createUserSchema = {
+  type:'object',
+  properties:{
+    name:{
+      type:'string',
+    },
+    age:{
+      type:'integer',
+      minimum:0,
+      maximum:150,
+    },
+  },
+  required:['name','age'],
+  additionalProperties:false,
+} as const;
+
+type CreateUserBody = FromSchema<typeof createUserSchema>;
+
+
+fastify.post(
+  '/users',
+  {
+    schema:{
+      body:createUserSchema,
+      response:{
+        201:{
+          type:'object',
+          properties:{
+            id:{type:'number'},
+            name:{type:'string'},
+            age:{type:'integer'},
+          },
+          required:['id','name','age'],
+        }
+      }
+    }
+  },
+  async (request,reply)=>{
+    const body = request.body as CreateUserBody;
+    const newUser = {id:0,...body};
+    reply.status(201).send(newUser);
+  },
+)
+
+
+
+
 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
