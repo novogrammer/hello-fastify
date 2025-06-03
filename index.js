0e2768b2b743fb780a2f3912184d5907252b72f2
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-03 11:21:13 +0900

    typescript対応

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
+"use strict";
 // Require the framework and instantiate it
-
-// ESM
-import Fastify from 'fastify'
-
-const fastify = Fastify({
-  logger: true
-})
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const fastify = (0, fastify_1.default)({
+    logger: true
+});
 // // CommonJs
 // const fastify = require('fastify')({
 //   logger: true
 // })
-
 // Declare a route
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
-
+    reply.send({ hello: 'world' });
+});
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    // Server is now listening on ${address}
+});
